aebaef9592bb95a10ce90a3b4d8a529c15e98391
Iliyan Vangelov <iliyan@st6.io>
2022-07-05 13:40:17 +0300

    feat: integrate xstyled
    - wrap App in ThemeProvider
    - use Preflight component to reset default styles (remove margins, default anchor element style etc.)
    - replace current anchor tag to demonstrate utility props comming from xstyled

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
+import { Preflight, ThemeProvider, defaultTheme, x } from '@xstyled/styled-components';
+
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+  <ThemeProvider theme={defaultTheme}>
+    <Preflight />
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <x.a
+          mt={3}
+          bg="yellow"
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </x.a>
+      </header>
+    </div>
+  </ThemeProvider>
 );
 
 export default App;
