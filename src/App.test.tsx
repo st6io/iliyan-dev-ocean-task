854305d0df6c91bec4acad6eab451e8cf9fe0083
Iliyan Vangelov <iliyan@st6.io>
2022-07-05 18:28:07 +0300

    test: add App component tests

---
+++ src/App.test.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider, defaultTheme } from '@xstyled/styled-components';
+
+import App from './App';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  BrowserRouter: ({ children }: { children: ReactNode }) => <div>BrowserRouter {children}</div>,
+}));
+
+jest.mock('@xstyled/styled-components', () => ({
+  ...jest.requireActual('@xstyled/styled-components'),
+  Preflight: () => <div>Preflight</div>,
+  ThemeProvider: jest.fn(),
+}));
+
+jest.mock('./Router', () => () => <div>Router</div>);
+
+describe('App', () => {
+  it.each(['Preflight', 'BrowserRouter', 'Router'])(
+    'should render "%s" component',
+    (componentName) => {
+      const { ThemeProvider: ActualThemeProvider } = jest.requireActual(
+        '@xstyled/styled-components',
+      );
+      (ThemeProvider as jest.Mock).mockImplementationOnce(ActualThemeProvider);
+      render(<App />);
+
+      expect(screen.getByText(componentName)).toBeInTheDocument();
+    },
+  );
+
+  it('should render ThemeProvider with default theme', () => {
+    render(<App />);
+
+    const [{ theme }] = (ThemeProvider as jest.Mock).mock.calls[0];
+
+    expect(theme).toMatchObject(defaultTheme);
+  });
+});
