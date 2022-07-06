7c7d8199bdca3adcb6517a877ae4a7e491c36e4a
Iliyan Vangelov <iliyan@st6.io>
2022-07-06 10:33:16 +0300

    feat: extend default theme with "light-gray" color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { Preflight, ThemeProvider, defaultTheme } from '@xstyled/styled-components';
+import { Preflight, ThemeProvider } from '@xstyled/styled-components';
 
 import Router from './Router';
+import theme from './theme';
 
 const App = () => (
-  <ThemeProvider theme={defaultTheme}>
+  <ThemeProvider theme={theme}>
     <Preflight />
 
     <BrowserRouter>
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { defaultTheme } from '@xstyled/styled-components';
+
+const theme = {
+  ...defaultTheme,
+
+  colors: {
+    ...defaultTheme.colors,
+    'light-grey': '#f8f8fa',
+  },
+};
+
+export default theme;
