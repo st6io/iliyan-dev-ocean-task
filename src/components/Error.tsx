a9dac1f33a3c23dc6a50fa99769632f5be3fce39
Iliyan Vangelov <iliyan@st6.io>
2022-07-08 17:04:00 +0300

    feat: add Error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import { SystemProps, x } from '@xstyled/styled-components';
+
+import Section from './Section';
+
+const Error = (props: SystemProps) => (
+  <Section title="Oops!" flex={1} textAlign="center" {...props}>
+    <Section.Text>Something went wrong.</Section.Text>
+    <Section.Text>Please try again later.</Section.Text>
+  </Section>
+);
+
+export default Error;
